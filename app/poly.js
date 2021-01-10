5ef6e47c772ca659b1bafc4d22803d0e8db8a42d
Martin Schneider <craftoid@k2g2.org>
2021-01-10 23:48:54 +0100

    Generalize polys

--- app/poly.js
+++ app/poly.js
@@ -3,34 +3,21 @@ import { PI, sin, cos } from './math.js';
 import * as PIXI from "./pixi.js";
 import hilite from './hilite.js';
 
-export default class Poly extends PIXI.Graphics {
+export class Poly extends PIXI.Graphics {
 
-  constructor(sides = 8, radius = 100, angle = 0, rotation = 0) {
-
+  constructor( pts ) {
+    
     super();
-
-    // range function
-    let range = (n) => [...Array(n).keys()];
-
-    // rotation offset
-    let rot = rotation + PI * angle / 180;
-
-    // create polygon points on a circle
-    const pts = range(sides).map( i => { 
-        const a = 2 * PI  * i / sides + rot;
-        return { x: radius * sin( a ), y: radius * cos( a ) }
-    });
-
     // draw poly
     const g = this;
 
     // drawing style
     const cline = 0x000000;
     const cfill = 0xffffff;
-    const alpha = 0.7 // somewhat transparent
-    const offset = 1.0; // inset lines
+    const alpha = 0.5 // somewhat transparent
+    const offset = 1; // inset lines
 
-    g.lineStyle(4, cline, alpha, offset);
+    g.lineStyle(2, cline, alpha, offset);
     g.beginFill(cfill, alpha);
     g.drawPolygon( pts );
     g.closePath();
@@ -43,5 +30,30 @@ export default class Poly extends PIXI.Graphics {
     hilite(g);
 
   }
+ 
+}
+
+export class RegularPoly extends Poly {
+  constructor(sides = 8, radius = 100, angle = 0, rotation = 0) {
+    super( polyPoints(0, 0, sides, radius, angle, rotation) );
+  }
+}
+
+// calculate corner-points of a polygon
+export function polyPoints(x = 0, y = 0, sides = 8, radius = 100, angle = 0, rotation = 0) {
+
+  // range function
+  let range = (n) => [...Array(n).keys()];
+
+  // rotation offset
+  let rot = rotation + PI * angle / 180;
+
+  // create polygon points on a circle
+  const pts = range(sides).map( i => { 
+      const a = 2 * PI  * i / sides + rot;
+      return { x: x + radius * sin( a ), y: y + radius * cos( a ) }
+  });
+
+  return pts;
 
 }

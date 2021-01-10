32435bd07236c62fa2af425b8177dcbf93200de6
Martin Schneider <craftoid@k2g2.org>
2021-01-10 11:57:49 +0100

    Cleanup

--- app/app.js
+++ app/app.js
@@ -1,9 +1,7 @@
 // Import all
 
-import TiledMap from './tiledmap.js';
 import { app, options } from "./pixi-app.js";
 import resize from "./resize.js";
-import { FloatingSprite, AnchoredSprite, StepperSprite } from "./sprite.js";
 import colorFilter from "./filters/colorFilter.js";
 import fullscreen from "./fullscreen.js";
 import Poly from "./poly.js";
@@ -31,34 +29,7 @@ function startup() {
   // dynamically resize the app to fixed aspect ratio
   resize(aspectRatio);
 
-  let layer = new TiledMap(app, "tilesheet", "tiledmap"); 
-  app.stage.addChild(layer);
-
-
-  // create a sprite the animates while moving
-  let pixelpal = new FloatingSprite("pixelpal", "walk", 2);
-  app.stage.addChild(pixelpal);
-
-  // create a sprite that uses anchors for animation
-  let littleguy = new AnchoredSprite("littleguy", "walk", 10);
-  app.stage.addChild(littleguy);
-
-  // create a sprite that lets us step through individual frames using arrow keys
-  let stepperguy = new StepperSprite("littleguy", "walk", 10);
-
-  // add a custom color filter, because we can ...
-  stepperguy.filters = [
-    colorFilter(0xff3333, 0x3333ff),
-    colorFilter(0xcc0000, 0x0000cc)
-  ];
-
-  hilite(littleguy);
-  hilite(stepperguy);
-  hilite(pixelpal);
-
-  app.stage.addChild(stepperguy);
-
-  // create some custom graphics and add it to the stage
+  // create some polys and add them to the stage
 
   const radius = 100;
   const poly = new Poly(8, radius, 22.5);

a08cb1ab91d5fca2bbd2da9d11f4de42a6055d80
Martin Schneider <craftoid@k2g2.org>
2021-01-13 00:55:53 +0100

    More math functions

--- app/math.js
+++ app/math.js
@@ -2,3 +2,20 @@
 export const cos = Math.cos;
 export const sin = Math.sin;
 export const PI = Math.PI;
+export const random = Math.random;
+
+export function lerp(a, b, t) {
+    return (1 - t) * a + t * b;
+}
+
+export function norm(t, a, b) {
+    return a + t * (b - a);
+}
+
+export function rand(min, max) {
+    return norm(random(), min, max);
+}
+
+export function dist(p1, p2) {
+    return Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2);
+}
